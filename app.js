d296cc711ebe495abb61fe802867dafded996c29
Renan Plathon <rplathon@gmail.com>
2016-04-01 20:22:06 -0300

    initial

---
+++ app.js
@@ -0,0 +1,43 @@
+var koa        = require('koa');
+var route      = require('koa-route');
+var bodyParser = require('koa-bodyparser');
+var cors       = require('koa-cors');
+var logger     = require('koa-logger');
+var mongoose   = require('mongoose');
+var jwt        = require('koa-jwt');
+var config     = require('./config');
+
+mongoose.connect(config.mongo);
+
+var app = koa();
+var env = process.env.NODE_ENV || 'development';
+if (env != 'test') app.use(logger())
+app.use(bodyParser());
+app.use(cors());
+
+var users = require('./services/users/user.controller');
+
+//jwt auth config middleware
+app.use(function *(next){
+  try {
+    yield next;
+  } catch (err) {
+    if (401 == err.status) {
+      this.status = 401;
+      this.body = 'Access denied';
+    } else
+      throw err;
+  }
+});
+
+//users
+app.use(route.post('/signin', users.auth));
+app.use(route.post('/signup', users.create));
+
+//set auth middleware
+app.use(jwt({ secret: config.secret }));
+
+if (env != 'test')
+  module.exports = app.listen(3001);
+else
+  app.listen(3001);
---
+++ services/users/user.model.js
@@ -0,0 +1,52 @@
+var mongoose   = require('mongoose');
+var timestamps = require('mongoose-timestamp');
+var Schema     = mongoose.Schema;
+
+/**
+* User Database Schema
+**/
+
+var UserSchema = new Schema({
+  name: { type: String,
+          required: true,
+          minlength: [ 3, 'Very short name' ],
+          maxlength: [ 64, 'Very long name' ]
+  },
+  email: { type: String,
+           required: true
+  },
+  type: { type: String },
+  password: { type: String,
+              required: true,
+              minlength: [ 3, 'Very short password' ],
+              maxlength: [ 64, 'Very long password' ]
+  }
+});
+
+/**
+* User Virtual Schema
+**/
+
+UserSchema
+  .virtual('withoutPassword')
+  .get(function () {
+    return {
+      _id:   this._id,
+      name: this.name,
+      email: this.email,
+      type: this.type
+    }
+  });
+
+/**
+* Timestamps plugin
+**/
+
+UserSchema.plugin(timestamps);
+
+/**
+* Create and exports model
+**/
+
+var UserModel = mongoose.model('User', UserSchema);
+module.exports = UserModel;
